2d545a6071b68ba19f8de59357f24338581d4399
adamskibicki <adam-skibicki@outlook.com>
2023-06-26 16:01:57 +0200

    Add check to not allow deleting last character status from user character

--- Progress.Application/Usecases/Status/Delete/DeleteCharacterStatusCommand.cs
+++ Progress.Application/Usecases/Status/Delete/DeleteCharacterStatusCommand.cs
@@ -1,8 +1,10 @@
 ﻿using FluentValidation;
 using LanguageExt;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Progress.Application.Common;
 using Progress.Application.Persistence;
+using Progress.Application.Persistence.Entities;
 
 namespace Progress.Application.Usecases.Status.Delete
 {
@@ -24,13 +26,25 @@ namespace Progress.Application.Usecases.Status.Delete
 
         protected override async Task<Either<Failure, Unit>> WrappedHandle(DeleteCharacterStatusCommand request, CancellationToken cancellationToken)
         {
-            var entityToRemove = dbContext.CharacterStatuses.Single(uc => uc.Id == request.Id);
+            var entityToRemove = dbContext.CharacterStatuses
+                .Include(cs => cs.UserCharacter)
+                .Single(cs => cs.Id == request.Id);
+
+            CheckIfUserCharacterHaveMoreThanOneCharacterStatus(entityToRemove);
 
             dbContext.CharacterStatuses.Remove(entityToRemove);
 
             await dbContext.SaveChangesAsync(cancellationToken);
 
             return Unit.Value;
+        }
+
+        private void CheckIfUserCharacterHaveMoreThanOneCharacterStatus(CharacterStatus entityToRemove)
+        {
+            if (entityToRemove.UserCharacter.CharacterStatuses.Count() == 1)
+            {
+                throw new Exception($"Cannot delete last {nameof(CharacterStatus)} from {nameof(UserCharacter)}");
+            }
         }
     }
 }
