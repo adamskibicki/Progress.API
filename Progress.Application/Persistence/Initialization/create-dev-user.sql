126e7fe8b6374492afda951162bfce65e324cd5a
adamskibicki <adam-skibicki@outlook.com>
2023-08-03 23:32:30 +0200

    Change to use dev user instead of superadmin for logging into dev db in container, add creating dev user when creating new db alongside db initialization, fixed problem with creating db tables in master db by default, added default project sql dialect

---
+++ Progress.Application/Persistence/Initialization/create-dev-user.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS 
+    (SELECT name  
+     FROM master.sys.server_principals
+     WHERE name = 'dev_user')
+BEGIN
+    CREATE LOGIN [dev_user] WITH PASSWORD = N'<dev_PASS12345!>'
+           
+    ALTER SERVER ROLE [dbcreator] ADD MEMBER [dev_user]
+END
